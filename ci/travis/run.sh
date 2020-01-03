b6788f2f1a400b60855b0865dbdc913af9d4115e
ossdev07 <39188636+ossdev07@users.noreply.github.com>
2020-01-03 17:07:46 +0530

    Update run.sh

--- ci/travis/run.sh
+++ ci/travis/run.sh
@@ -5,10 +5,10 @@ if [[ "$FLAKE8" == "true" ]]; then
     dirname="$(find /opt/conda/lib -iname python* -type d -maxdepth 1)"
     cp bdist_conda.py $dirname/distutils/command
     pushd tests/bdist-recipe && python setup.py bdist_conda && popd
-    sudo conda build --help
-    sudo conda build --version
-    sudo conda build conda.recipe --no-anaconda-upload
-    sudo conda create -n _cbtest conda-build glob2
+    conda build --help
+    conda build --version
+    conda build conda.recipe --no-anaconda-upload
+    conda create -n _cbtest conda-build glob2
     # because this is a file, conda is not going to process any of its dependencies.
     sudo conda install -n _cbtest $(conda render --output conda.recipe | head -n 1)
     source activate _cbtest
